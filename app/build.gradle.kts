0691d4c4ff124f6c9a1e4526e280772b6f2ee341
natdfernandes <nataliadiasfernande@gmail.com>
2026-07-12 11:53:50 -0300

    test strategy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ android {
 }
 
 dependencies {
+    implementation (platform("androidx.compose:compose-bom:2023.06.01"))
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.foundation:foundation-layout:1.11.4")
@@ -75,6 +76,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
+
+    testImplementation("junit:junit:4.13.2")
 
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
